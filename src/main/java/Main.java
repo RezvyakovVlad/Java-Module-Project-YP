38459ce279fd9441bd17ea5795a4f32fb846f557
RezvyakovVlad <rezvyakovvlad@gmail.com>
2025-05-10 17:08:26 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         List<Car> cars = new ArrayList<>();
 
         System.out.println("Введите данные 3 автомобилей:");
